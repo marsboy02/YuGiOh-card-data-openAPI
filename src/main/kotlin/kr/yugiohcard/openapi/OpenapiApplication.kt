20511860f6b58913388f84c1161ccc44bc14199d
Marsboy02 <marsboy0619@gmail.com>
2023-06-01 21:51:33 +0900

    initial commit

---
+++ src/main/kotlin/kr/yugiohcard/openapi/OpenapiApplication.kt
@@ -0,0 +1,11 @@
+package kr.yugiohcard.openapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenapiApplication>(*args)
+}
---
+++ src/test/kotlin/kr/yugiohcard/openapi/OpenapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.yugiohcard.openapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
