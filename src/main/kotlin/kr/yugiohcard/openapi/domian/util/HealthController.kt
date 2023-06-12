0342185193851116c02d9689e9187591806c89be
Marsboy02 <marsboy0619@gmail.com>
2023-06-12 14:11:52 +0900

    fix: 기본 url을 통해 헬스체크하도록 변경

--- src/main/kotlin/kr/yugiohcard/openapi/domian/util/HealthController.kt
+++ src/main/kotlin/kr/yugiohcard/openapi/domian/util/HealthController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping
 class HealthController {
     @GetMapping
     fun getHealth(): String {
