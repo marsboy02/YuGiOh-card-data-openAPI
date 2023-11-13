e968810756c25f87eaca654c9b494554d21dd46e
Marsboy02 <marsboy0619@gmail.com>
2023-11-13 18:32:07 +0900

    ci: 배포되는 환경에서 swagger cors 에러 수정

--- src/main/kotlin/kr/yugiohcard/openapi/OpenapiApplication.kt
+++ src/main/kotlin/kr/yugiohcard/openapi/OpenapiApplication.kt
@@ -1,11 +1,17 @@
 package kr.yugiohcard.openapi
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+    servers = [Server(url = "/", description = "Default Server url")]
+)
 @SpringBootApplication
 class OpenapiApplication
 
 fun main(args: Array<String>) {
     runApplication<OpenapiApplication>(*args)
 }
+
